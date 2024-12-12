77cd5f73d27416c3e18b7adcc32a7845cfd3a639
smallnest <smallnest@gmail.com>
2024-12-12 23:49:16 +0800

    add a simple sqlmock

--- sqlmock/compare_test.go
+++ sqlmock/compare_test.go
@@ -3,14 +3,11 @@ package sqlmock
 import "testing"
 
 func TestSQLCompare(t *testing.T) {
-	// 语义相等的查询
 	sql1 := "SELECT id, name FROM users WHERE age > 18 ORDER BY name"
 	sql2 := "SELECT name, id FROM users WHERE age > 18 ORDER BY name"
 
-	// 语义不等的查询
 	sql3 := "SELECT id FROM users WHERE age > 21"
 
-	// 比较
 	if !CompareSQL(sql1, sql2) {
 		t.Errorf("SQL statements should be semantically equal")
 	}
